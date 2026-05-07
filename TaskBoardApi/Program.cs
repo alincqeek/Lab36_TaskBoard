f3982ad841a68b0d6ac3d10d5350c7b410f23319
alincqeek <a72658695@gmail.com>
2026-05-07 20:22:50 +0300

    Step 3: TasksController with full CRUD

--- TaskBoardApi/Program.cs
+++ TaskBoardApi/Program.cs
@@ -4,14 +4,14 @@ using TaskBoardApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
 .AddJsonOptions(options => {
-options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
 builder.Services.AddCors(options => {
-options.AddPolicy("AllowAll", policy => {
-policy.AllowAnyOrigin()
-.AllowAnyMethod()
-.AllowAnyHeader();
-});
+    options.AddPolicy("AllowAll", policy => {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
 });
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlite("Data Source=taskboard.db"));
@@ -19,12 +19,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 using (var scope = app.Services.CreateScope()) {
-var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-db.Database.Migrate();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
 if (app.Environment.IsDevelopment()) {
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
